9e6c0d0deea7f994406d523b369699509b12fced
SzGeri25 <szabo.gergely25@gmail.com>
2026-08-24 11:10:48 +0200

    Refactor App component to use React fragments instead of a div

--- 01-react-essentials/src/App.jsx
+++ 01-react-essentials/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -48,7 +48,7 @@ function App() {
           )}
         </section>
       </main>
-    </div>
+    </>
   );
 }
 
